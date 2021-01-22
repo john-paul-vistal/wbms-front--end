248d5e6c1deb25d4a906210ab31d8f9f0cdfd80c
2ndyrGroupA <2ndyrGroupA@pn.local>
2021-01-23 00:16:24 +0800

    Change Layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { LoginComponent } from './login/login.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { CreateTransactionComponent } from './create-transaction/create-transaction.component';
 import { TransactionComponent } from './transaction/transaction.component';
@@ -9,6 +10,10 @@ import { EmployeeComponent } from './employee/employee.component';
 import { SettingsComponent } from './settings/settings.component';
 
 const routes: Routes = [
+  {
+    path: '',
+    component: LoginComponent
+  },
   {
     path: 'dashboard',
     component: LandingPageComponent
